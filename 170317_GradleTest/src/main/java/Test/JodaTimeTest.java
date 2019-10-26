24536a8dff48f4168b3f3aa8aa88159fe5936e21
ldasm <ldasm@DESKTOP-60300RG>
2019-10-26 18:32:02 +0900

    Gradle 테스트

---
+++ 170317_GradleTest/src/main/java/Test/JodaTimeTest.java
@@ -0,0 +1,10 @@
+package Test;
+
+import org.joda.time.DateTime;
+
+public class JodaTimeTest {
+	public static void main(String[] args) {
+		DateTime time = new DateTime();
+		System.out.println(DateTime.now());
+	}
+}
